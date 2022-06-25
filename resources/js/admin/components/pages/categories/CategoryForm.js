bf3eb0157d791667639b7242e4d07623785c4d8a
Jitendra personal <jks0586@gmail.com>
2022-06-25 08:17:12 +0530

    update

--- resources/js/admin/components/pages/categories/CategoryForm.js
+++ resources/js/admin/components/pages/categories/CategoryForm.js
@@ -1,142 +1,264 @@
-import React from "react";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import React from 'react'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Alert from 'react-bootstrap/Alert'
 
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 class CategoryForm extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor (props) {
+        super(props)
         // console.log(this.props.categories.category.title);
         // console.log(this.props);
         this.state = {
-            title: "",
-            description: "",
-            image: "",
-            slug: "",
-        };
+            title: '',
+            description: '',
+            image: '',
+            slug: '',
+            imagePreview: '',
+            imagePreviewUrl: '',
+            formErrors: {},
+        }
 
-        this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.handleImageChange = this.handleImageChange.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this)
+        this.handleImageChange = this.handleImageChange.bind(this)
+        this.handleSlugChange = this.handleSlugChange.bind(this)
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        // this.categoryValidate = this.categoryValidate.bind(this)
     }
 
-    handleTitleChange(e) {
-        this.setState({ title: e.target.value });
+    handleTitleChange (e) {
+        this.setState({ title: e.target.value })
         // console.log(this.state);
-        e.preventDefault();
+        e.preventDefault()
     }
-    handleImageChange(e) {
+    handleImageChange (e) {
         // alert("aaaaaaaa");
         // console.log(e.target.files[0]);
-        this.state({'image': e.target.files[0]});
-        this.previewFile(this.state);
-        e.preventDefault();
+        this.setState({ image: e.target.files[0] })
+        this.previewFile(e.target.files[0])
+
+        setTimeout(function () {
+            let imgset = document.getElementById('imagePreview');
+            this.setState({title : imgset.getAttribute('src')})
+        }, 1000)
+
+        e.preventDefault()
     }
-    previewFile(file) {
-        const reader = new FileReader();
+    handleSlugChange (e) {
+        this.setState({ slug: e.target.value })
+        e.preventDefault()
+    }
+    handleDescriptionChange (e) {
+        this.setState({ description: e.target.value })
+        e.preventDefault()
+    }
+    previewFile (file) {
+        const reader = new FileReader()
+
         reader.addEventListener(
-          "load",
-          function () {
-            // preview.src = reader.result;
-            this.imagePreview = reader.result;
-            // console.log(reader.result);
-            let imgset = document.getElementById("imagePreview");
-            imgset.setAttribute("src", reader.result);
-            imgset.setAttribute("width", 50);
-            imgset.setAttribute("height", 50);
-          },
-          false
-        );
-  
+            'load',
+            function () {
+                // preview.src = reader.result;
+                // console.log(reader.result);
+                let imgset = document.getElementById('imagePreview')
+                imgset.setAttribute('src', reader.result)
+                imgset.setAttribute('width', 50)
+                imgset.setAttribute('height', 50)
+                // 
+            },
+            false
+        )
+
         if (file) {
-          reader.readAsDataURL(file);
+            reader.readAsDataURL(file)
         }
-      }
-    handleSubmit(e) {
-        console.log("yyyyyy");
-        e.preventDefault();
+    }
+    categoryValidate (e) {
+        // console.log('aaaaaayyyyyyy', this.state)
+        // const formErrors={};
+        let fieldValidationErrors = this.state.formErrors
+        Object.keys(this.state).forEach((value, index) => {
+            // console.log(value);
+            // console.log(fieldValidationErrors.value);
+
+            if (
+                value == 'title' &&
+                (this.state[value] == '' || this.state[value] == undefined)
+            ) {
+                fieldValidationErrors[value] =
+                    value + ' Field could not be empty'
+            }
+            if (
+                value == 'description' &&
+                (this.state[value] == '' || this.state[value] == undefined)
+            ) {
+                fieldValidationErrors[value] =
+                    value + ' Field could not be empty'
+            }
+            if (
+                value == 'image' &&
+                (this.state[value] == '' || this.state[value] == undefined)
+            ) {
+                fieldValidationErrors[value] =
+                    value + ' Field could not be empty'
+            }
+            if (
+                value == 'slug' &&
+                (this.state[value] == '' || this.state[value] == undefined)
+            ) {
+                fieldValidationErrors[value] =
+                    value + ' Field could not be empty'
+            }
+
+            // console.log(value,this.state[value]);
+        })
+
+        // console.log(fieldValidationErrors);
+
+        this.setState({ formErrors: fieldValidationErrors })
+
+        console.log(this.state.formErrors)
+
+        if (this.state.formErrors) {
+            return false
+        } else {
+            return true
+        }
+
+        // console.log(formErrors);
+    }
+    handleSubmit (e) {
+        e.preventDefault()
+        // console.log("yyyyyy");
+        if (this.categoryValidate(e)) {
+            const postdata = {}
+            Object.keys(this.state).forEach((value, index) => {
+                postdata.index = value
+            })
+        }
+
+        // console.log(postdata);
     }
 
-    render() {
+    render () {
         return (
             <>
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group className="mb-3" controlId="formTitle">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter Title"
-                        name="title"
-                        value={this.state.value}
-                        onChange={this.handleTitleChange}
-                    />
-                    <Form.Text className="text-muted">
-                        Please fill category title
-                    </Form.Text>
-                </Form.Group>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group className='mb-3' controlId='formTitle'>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder='Enter Title'
+                            name='title'
+                            value={this.state.value}
+                            onChange={this.handleTitleChange}
+                        />
+                        <Alert
+                            show={this.state.formErrors['title'] ? true : false}
+                            variant='danger'
+                        >
+                            {this.state.formErrors['title']}
+                        </Alert>
+                        <Form.Text className='text-muted'>
+                            Please fill category title
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formDescription">
-                    <Form.Label>Description</Form.Label>
-                    <CKEditor
-                        editor={ClassicEditor}
-                        data="<p>Hello from CKEditor 5!</p>"
-                        onReady={(editor) => {
-                            // You can store the "editor" and use when it is needed.
-                            console.log("Editor is ready to use!", editor);
-                        }}
-                        onChange={(event, editor) => {
-                            const data = editor.getData();
-                            console.log({ event, editor, data });
-                        }}
-                        onBlur={(event, editor) => {
-                            console.log("Blur.", editor);
-                        }}
-                        onFocus={(event, editor) => {
-                            console.log("Focus.", editor);
-                        }}
-                    />
-                    <Form.Text className="text-muted">
-                        Please Fill Category Description
-                    </Form.Text>
-                </Form.Group>
+                    <Form.Group className='mb-3' controlId='formDescription'>
+                        <Form.Label>Description</Form.Label>
+                        <CKEditor
+                            editor={ClassicEditor}
+                            data='<p>Hello from CKEditor 5!</p>'
+                            onReady={editor => {
+                                const data = editor.getData()
+                                this.setState({ description: data })
+                                // You can store the "editor" and use when it is needed.
+                                // console.log("Editor is ready to use!", editor);
+                            }}
+                            onChange={(event, editor) => {
+                                const data = editor.getData()
+                                this.setState({ description: data })
+                                // console.log({ event, editor, data });
+                            }}
+                            onBlur={(event, editor) => {
+                                const data = editor.getData()
+                                this.setState({ description: data })
+                                // console.log("Blur.", editor);
+                            }}
+                            onFocus={(event, editor) => {
+                                const data = editor.getData()
+                                this.setState({ description: data })
+                                // console.log("Focus.", editor);
+                            }}
+                        />
+                        <Alert
+                            show={
+                                this.state.formErrors['description']
+                                    ? true
+                                    : false
+                            }
+                            variant='danger'
+                        >
+                            {this.state.formErrors['description']}
+                        </Alert>
+                        <Form.Text className='text-muted'>
+                            Please Fill Category Description
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formImage">
-                    <Form.Label>Image</Form.Label>
-                    <Form.Control
-                        type="file"
-                        placeholder="Select Image"
-                        name="image"
-                        onChange={this.handleImageChange}
-                    />
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top"  id="imagePreview" src="holder.js/100px180?text=Image cap" />
-                    </Card>
-                    
-                    <Form.Text className="text-muted">
-                        Please select you image
-                    </Form.Text>
-                </Form.Group>
+                    <Form.Group className='mb-3' controlId='formImage'>
+                        <Form.Label>Image</Form.Label>
+                        <Form.Control
+                            type='file'
+                            placeholder='Select Image'
+                            name='image'
+                            onChange={this.handleImageChange}
+                        />
+                        <Card style={{ width: '50px' }}>
+                            <Card.Img id='imagePreview' src='' />
+                        </Card>
+                        <Alert
+                            show={this.state.formErrors['image'] ? true : false}
+                            variant='danger'
+                        >
+                            {this.state.formErrors['image']}
+                        </Alert>
+                        <div className='clearfix'></div>
 
-                <Form.Group className="mb-3" controlId="formSlug">
-                    <Form.Label>Slug</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter Slug"
-                        name="slug"
-                    />
-                    <Form.Text className="text-muted">
-                        Please fill category slug
-                    </Form.Text>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                        <Form.Text className='text-muted'>
+                            Please select you image
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='formSlug'>
+                        <Form.Label>Slug</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder='Enter Slug'
+                            name='slug'
+                            onChange={this.handleSlugChange}
+                        />
+                        <Alert
+                            show={this.state.formErrors['slug'] ? true : false}
+                            variant='danger'
+                        >
+                            {this.state.formErrors['slug']}
+                        </Alert>
+                        <Form.Text className='text-muted'>
+                            Please fill category slug
+                        </Form.Text>
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>
+                        Submit
+                    </Button>
+                </Form>
             </>
-        );
+        )
     }
 }
 
-export default CategoryForm;
+export default CategoryForm
